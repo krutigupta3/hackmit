e6763eb7dc7a2a19a937168c38782e8b70c605cf
Kruti Gupta <73044001+krutigupta3@users.noreply.github.com>
2022-10-01 22:05:12 -0400

    pop up for chart and graphs

---
+++ src/DataGas.jsx
@@ -0,0 +1,44 @@
+export const DataGas = [
+    {
+      id: 1,
+      day: 'Monday',
+      userGain: 3.2,
+      userLost: 823,
+    },
+    {
+      id: 2,
+      day: 'Tuesday',
+      userGain: 2.9,
+      userLost: 345,
+    },
+    {
+      id: 3,
+      day: 'Wednesday',
+      userGain: 2.5,
+      userLost: 555,
+    },
+    {
+      id: 4,
+      day: 'Thursday',
+      userGain: 2.6,
+      userLost: 4555,
+    },
+    {
+      id: 5,
+      day: 'Friday',
+      userGain: 4.7,
+      userLost: 234,
+    },
+    {
+        id: 6,
+        day: 'Saturday',
+        userGain: 4.9,
+        userLost: 234,
+      },
+      {
+        id: 7,
+        day: 'Sunday',
+        userGain: 2.5,
+        userLost: 234,
+      },
+  ];
--- src/DataWater.jsx
+++ src/DataWater.jsx
@@ -1,32 +1,44 @@
-export const UserData = [
+export const DataWater = [
     {
       id: 1,
       day: 'Monday',
-      userGain: 80000,
+      userGain: 110.8,
       userLost: 823,
     },
     {
       id: 2,
       day: 'Tuesday',
-      userGain: 45677,
+      userGain: 120.7,
       userLost: 345,
     },
     {
       id: 3,
       day: 'Wednesday',
-      userGain: 78888,
+      userGain: 97.5,
       userLost: 555,
     },
     {
       id: 4,
       day: 'Thursday',
-      userGain: 90000,
+      userGain: 123.4,
       userLost: 4555,
     },
     {
       id: 5,
       day: 'Friday',
-      userGain: 4300,
+      userGain: 151.9,
       userLost: 234,
     },
+    {
+        id: 6,
+        day: 'Saturday',
+        userGain: 148.7,
+        userLost: 234,
+      },
+      {
+        id: 7,
+        day: 'Sunday',
+        userGain: 125.4,
+        userLost: 234,
+      },
   ];
